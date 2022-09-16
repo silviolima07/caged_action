156ae992cb920764ea5e60e43a74e5eb3a789c53
Silvio Lima <silviolima07@gmail.com>
2022-09-16 17:04:38 -0300

    caged_tab5

--- scripts/caged_tab5.py
+++ scripts/caged_tab5.py
@@ -81,5 +81,7 @@ df_tab5['Saldos'] = df_tab5['Saldos'].astype('int32')
 df_tab5['Variação Relativa (%)'] = df_tab5['Variação Relativa (%)'].replace('----',0)
 df_tab5['Variação Relativa (%)'] = np.round(df_tab5['Variação Relativa (%)'],2)
 
+df_tab5.rename(columns={'Mês': 'Data'}, inplace=True)
+
 #df_tab5.to_csv("df_caged_tab5_"+data+'.csv', index=False, encoding='utf-8')
 df_tab5.to_csv("df_caged_tab5.csv", index=False, encoding='utf-8')
