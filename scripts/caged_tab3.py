5a4e67365946b496a3eb22893ff43ab6f1e08384
Silvio Lima <silviolima07@gmail.com>
2022-09-27 16:19:30 -0300

    Update caged_tab3.py

--- scripts/caged_tab3.py
+++ scripts/caged_tab3.py
@@ -67,5 +67,5 @@ df_tab3['Variação Relativa (%)'] = np.round(df_tab3['Variação Relativa (%)']
 df_tab3.rename(columns={'UF':'uf', 'Município':'municipio', 'Admissões':'admissoes', 'Desligamentos': 'desligamentos', 'Saldos': 'saldos', 'Variação Relativa (%)': 'variacao_relativa'}, inplace=True)
 #teste = df_tab3[['uf', 'municipio']]
 #df_tab3.to_csv("df_caged_tab3_"+data+".csv", index=False, encoding='utf-8')
-df_tab3.to_csv("df_caged_tab3.csv", index=False, encoding='utf-8')
+df_tab3.to_csv("df_caged_tab3.csv", index=False, encoding='utf-8', header=False)
 df_tab3.to_parquet("df_caged_tab3.parquet",engine='pyarrow')
