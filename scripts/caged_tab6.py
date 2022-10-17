ab14fa62d88f8b08185c52ac410e6c22c9786edf
Silvio Lima <silviolima07@gmail.com>
2022-10-17 13:45:31 -0300

    Update caged_tab6.py

--- scripts/caged_tab6.py
+++ scripts/caged_tab6.py
@@ -100,7 +100,7 @@ temp = []
 for var in atividades:
     word = str(var).replace('[','').replace(']','')
     print("Atividades:\n",word)
-    temp.append(var)
+    temp.append(word)
 
 print("Atividades sem []:\n", temp)
           
